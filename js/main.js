53e713cf56e25bcc5487e1dab3c36094f77e1ae2
Marjorie Figueroa <estefi1515@gmail.com>
2015-09-05 12:02:06 -0700

    Skew y Animaciones

--- js/main.js
+++ js/main.js
@@ -47,4 +47,52 @@ $(document).ready(function () {
     $('header ul li a#about, header ul li a#work').click(function(e) {
         e.preventDefault();
     });
+
+    var animation = "animated swing";
+    var animEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    $("#picture").hover(function(){
+
+    	 $(this).addClass(animation).one(animEvents, function(){
+        	$(this).removeClass(animation);
+        });
+    });
+    $("#occupation").hide();
+    $("#name").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    	$("#occupation").show();
+    	$("#occupation").addClass('animated slideInLeft');
+    });
 });
+
+$(window).scroll(function(){
+    //checkAnimation();
+});
+
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+
+function checkAnimation() {
+    var $elem1 = $('#picture');
+
+    // If the animation has already been started
+    if ($elem1.hasClass('bounce')) return;
+
+    if (isElementInViewport($elem1)) {
+        // Start the animation
+        $elem1.addClass('animated bounce').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        	$(this).removeClass('animated bounce');
+        });
+    }
+}
