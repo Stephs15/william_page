4edc52c4ac34a80aad1d0649f7aedfe90eb68071
Marjorie Figueroa <estefi1515@gmail.com>
2015-09-04 23:39:15 -0700

    FIXES

--- js/main.js
+++ js/main.js
@@ -15,3 +15,36 @@ headroom.init();
 // to destroy
 headroom.destroy();
 });
+
+function scrollToSection(section){
+    $('html, body').animate({
+        scrollTop: $(section).offset().top
+    }, 1000);
+}
+
+$(document).ready(function () {
+    /*Smooth scrolling testing*/
+    $('#work').click(function(e) {
+        e.preventDefault();
+        scrollToSection('#myWorkA');
+    });
+
+    $('#about').click(function(e) {
+        e.preventDefault();
+        scrollToSection('#aboutMeA');
+    });
+
+    $('#contact').click(function(e) {
+        e.preventDefault();
+        scrollToSection('#contactA');
+    });
+
+    $('#testimonials').click(function(e) {
+        e.preventDefault();
+        scrollToSection('#testimonialsA');
+    });
+
+    $('header ul li a#about, header ul li a#work').click(function(e) {
+        e.preventDefault();
+    });
+});
