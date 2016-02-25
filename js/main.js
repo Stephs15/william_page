8566f9f57d6ce3e2338b6b5d371ed9b503fd32cd
Marjorie Figueroa <estefi1515@gmail.com>
2016-02-24 20:55:35 -0430

    Arreglos en media queries

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
         scrollToSection('#testimonialsA');
     });
 
-    $('#clients').click(function(e) {
+    $('#clientsl').click(function(e) {
         e.preventDefault();
         scrollToSection('#clientsA');
     });
@@ -465,27 +465,27 @@ jQuery(document).ready(function() {
 });
 
 
-$(document).ready(function () {
-    $(document).on("scroll", onScroll);
+// $(document).ready(function () {
+//     $(document).on("scroll", onScroll);
     
-    //smoothscroll
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
+//     //smoothscroll
+//     $('a[href^="#"]').on('click', function (e) {
+//         e.preventDefault();
+//         $(document).off("scroll");
         
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
+//         $('a').each(function () {
+//             $(this).removeClass('active');
+//         })
+//         $(this).addClass('active');
       
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
-});
+//         var target = this.hash,
+//             menu = target;
+//         $target = $(target);
+//         $('html, body').stop().animate({
+//             'scrollTop': $target.offset().top+2
+//         }, 500, 'swing', function () {
+//             window.location.hash = target;
+//             $(document).on("scroll", onScroll);
+//         });
+//     });
+// });
